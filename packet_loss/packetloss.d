8e2f3c5718c1b91a44c5165b6950da983567906d
klasbo <klasbo@gmail.com>
2022-03-16 14:04:52 +0100

    Update packet loss script: helpstr, OUTPUT chain
    Now also applies packet loss to the OUTPUT chain. This makes it so messages can be lost also by the sender, not just the receiver.

--- packet_loss/packetloss.d
+++ packet_loss/packetloss.d
@@ -1,6 +1,13 @@
-/+
+
+
+import std;
+
+
+auto helpstr = `
 Oversimplified packet loss script.
 This program intentionally has significant limitations. Use 'iptables' directly for advanced operations.
+
+Remember to run this program with 'sudo'
 
 Options:
 Either long or short options are allowed
@@ -15,39 +22,48 @@ Either long or short options are allowed
         Remove all packet loss rules
         
 Examples:
-    packetloss -f
+    sudo packetloss -f
         Removes all packet loss rules, disabling packet loss
         
-    packetloss -p 12345,23456,34567 -r 0.25
+    sudo packetloss -p 12345,23456,34567 -r 0.25
         Applies 25% packet loss to ports 12345, 23456, and 34567
         
-    packetloss -n executablename -r 0.25
+    sudo packetloss -n executablename -r 0.25
         Applies 25% packet loss to all ports used by all programs named "executablename"
         
-    packetloss -p 12345 -n executablename -r 0.25
+    sudo packetloss -p 12345 -n executablename -r 0.25
         Also applies 25% packet loss to port 12345
 
-    packetloss -n executablename -f
+    sudo packetloss -n executablename -f
         Lists ports used by "executablename", but does not apply packet loss
         
-+/
-
-import std;
+`;
 
 void main(string[] args){
 
+    bool        help;
     bool        flush;
     ushort[]    ports;
     string      name;
     double      prob;
+    
+    string[]    chains = ["INPUT", "OUTPUT"];
+    string[]    protocols = ["udp", "tcp"];
 
     arraySep = ",";
-    args.getopt(std.getopt.config.passThrough,
+    string[] argsCpy = args;
+    argsCpy.getopt(std.getopt.config.passThrough,
+        "h|help",               &help,
         "f|flush",              &flush,
         "p|port|ports",         &ports,
         "n|name",               &name,
         "r|rate|probability",   &prob,
     );
+    
+    if(help || args.length == 1){
+        helpstr.writeln;
+        return;
+    }
     
     if(name != string.init){
         auto pids = format!("pidof %s")(name).executeShell.output.split;
@@ -65,20 +81,21 @@ void main(string[] args){
     }
     
     writeln("Flushing iptables chains...");
-    executeShell("iptables -F INPUT");
+    foreach(chain; chains){
+        format!("iptables -F %s")(chain).executeShell;
+    }
     
     if(flush){
         goto done;
     }
     
-    
-    foreach(proto; ["udp", "tcp"]){
+    foreach(chainProtoPair; cartesianProduct(chains, protocols)){
         string command = 
-            format!("iptables -A INPUT -p %s ")(proto)
+            format!("iptables -A %s -p %s ")(chainProtoPair[0], chainProtoPair[1])
             ~ format!("-m multiport --destination-ports %-(%d,%) ")(ports) 
             ~ format!("-m statistic --mode random --probability %f ")(prob) 
             ~ "-j DROP";
-                
+                    
         writefln!("Performing command:\n  %s")(command);
         
         auto r = command.executeShell;
@@ -86,10 +103,11 @@ void main(string[] args){
             writefln!("Error:\n%-(  %s\n%)")(r.output.split("\n"));
             goto done;
         }
+        
     }
     
 done:
-    writefln!("Result of 'iptables -L':\n%-(  %s\n%)")(
+    writefln!("\n\nResult of 'iptables -L':\n\n%-(  %s\n%)")(
         executeShell("iptables -L").output.split("\n")
     );
 
